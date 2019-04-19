1825c16ef86a5f15e3ea34a2619bcd682f03778c
Jaakko Alajoki <jaamo@Jaakkos-MacBook-Pro.local>
2019-04-19 09:42:46 +0300

    First version of the client. Reorganizing code.

---
+++ assets/js/SocketFight.ts
@@ -0,0 +1,89 @@
+import Phaser from 'phaser';
+import {Socket} from 'phoenix';
+
+/**
+ * Root class for SocketFight game component.
+ */
+export default class SocketFight {
+  /**
+   * Configure and create the game.
+   */
+  constructor() {
+    // Configurations.
+    const config = {
+      type: Phaser.AUTO,
+      width: 1080,
+      height: 720,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: {y: 200},
+        },
+      },
+      scene: {
+        preload: this.preload,
+        create: this.create,
+      },
+    };
+
+    // Init game.
+    this.game = new Phaser.Game(config);
+
+    // Init inputs.
+    document.addEventListener('keyup', e => this.onKeyPress(e));
+
+    // Init socket.
+    this.socket = new Socket('/socket', {params: {token: window.userToken}});
+    this.socket.connect();
+    this.channel = this.socket.channel('game:lobby', {});
+    this.channel
+      .join()
+      .receive('ok', resp => {
+        console.log('Joined successfully', resp);
+      })
+      .receive('error', resp => {
+        alert(
+          'Server connection failed. Nothing will work. Rather than debugging please go out and do something meaningful.',
+        );
+        console.log('Unable to join', resp);
+      });
+    this.channel.on('new_msg', this.receiver);
+  }
+
+  /**
+   * Define assets to be preloaded.
+   */
+  preload() {
+    this.load.setBaseURL('/');
+    this.load.image('map', 'images/map.jpg');
+    this.load.image('tank', 'images/tank.png');
+  }
+
+  /**
+   * Game initializer function called when all assets are loaded.
+   */
+  create() {
+    this.add.image(540, 360, 'map');
+    this.add.image(540, 360, 'tank');
+  }
+
+  /**
+   * Receive data from backend.
+   */
+  receiver(payload) {
+    console.log(payload.body);
+  }
+
+  /**
+   * Handle keypress.
+   */
+  onKeyPress(e) {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+      // a, left
+      case 67:
+        this.channel.push('new_msg', {body: 'left'});
+        break;
+    }
+  }
+}
