ad500110498b5315ddf457dff0506aa2887b2b69
Jaakko Alajoki <jaamo@Jaakkos-MacBook-Pro.local>
2019-04-28 21:02:51 +0300

    Prepared the first test case for collisions.

--- lib/socketfight/collision_detector.ex
+++ lib/socketfight/collision_detector.ex
@@ -1,5 +1,6 @@
 defmodule Socketfight.CollisionDetector do
 
+  # https://cp-algorithms.com/geometry/circle-line-intersection.html
   def collides(player, obstable) do
     false
   end
--- test/socketfight/game_state_test.exs
+++ test/socketfight/game_state_test.exs
@@ -2,7 +2,30 @@ defmodule AssertionTest do
   use ExUnit.Case, async: true
   alias Socketfight.CollisionDetector
 
+  """
+
+            *  *
+         *        *
+        *          *
+        *          *
+         *        *
+            *  *
+
+
+  ------------------------
+
+  """
   test "no collision" do
-    assert CollisionDetector.collides(0, 0)
+
+    player = %{
+      id: 0,
+      state: %{
+        x: 540,
+        y: 360,
+      }
+    }
+    obstacle = %{a: %{x: 100, y: 100}, b: %{x: 800, y: 100}}
+
+    assert CollisionDetector.collides(player, obstacle)
   end
 end
