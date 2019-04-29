7ccba638d461202e60d5020b7ebc6d87a6110270
Jaakko Alajoki <jaamo@Jaakkos-MacBook-Pro.local>
2019-04-29 21:04:18 +0300

    Collision detection function.

--- lib/socketfight/collision_detector.ex
+++ lib/socketfight/collision_detector.ex
@@ -1,8 +1,63 @@
 defmodule Socketfight.CollisionDetector do
+  # https://stackoverflow.com/a/1088058
+  # https://i.stack.imgur.com/P556i.png
+  def collides(player, obstacle) do
+    # Prepare variables.
+    # Line start end end.
+    ax = obstacle.a.x
+    ay = obstacle.a.y
+    bx = obstacle.b.x
+    by = obstacle.b.y
 
-  # https://cp-algorithms.com/geometry/circle-line-intersection.html
-  def collides(player, obstable) do
-    false
+    # Circle.
+    cx = player.state.x
+    cy = player.state.y
+    r = player.radius
+
+    # Compute the euclidean distance between A and B.
+    lab = :math.sqrt(round(:math.pow(bx - ax, 2)) + round(:math.pow(by - ay, 2)))
+
+    # compute the direction vector D from A to B
+    dx = (bx - ax) / lab
+    dy = (by - ay) / lab
+
+    # The equation of the line AB is x = Dx*t + Ax, y = Dy*t + Ay with 0 <= t <= LAB.
+
+    # Compute the distance between the points A and E, where
+    # E is the point of AB closest the circle center (Cx, Cy)
+    t = dx * (cx - ax) + dy * (cy - ay)
+
+    # Compute the coordinates of the point E.
+    ex = t * dx + ax
+    ey = t * dy + ay
+
+    # Compute the euclidean distance between E and C.
+    lec = :math.sqrt(:math.pow(ex - cx, 2) + :math.pow(ey - cy, 2))
+
+    # Test if the line intersects the circle.
+    cond do
+      lec < r ->
+        # Compute distance from t to circle intersection point.
+        # _dt = :math.sqrt(:math.pow(r, 2) - :math.pow(lec, 2))
+
+        # Compute first intersection point.
+        # _fx = (t - dt) * dx + ax
+        # _fy = (t - dt) * dy + ay
+
+        # Compute second intersection point.
+        # _gx = (t + dt) * dx + ax
+        # _gy = (t + dt) * dy + ay
+
+        true
+
+      # else test if the line is tangent to circle
+      lec == r ->
+        # tangent point to circle is E
+        true
+
+      true ->
+        # line doesn't touch circle
+        false
+    end
   end
-
 end
--- test/socketfight/game_state_test.exs
+++ test/socketfight/game_state_test.exs
@@ -2,29 +2,47 @@ defmodule AssertionTest do
   use ExUnit.Case, async: true
   alias Socketfight.CollisionDetector
 
-  """
-
-            *  *
-         *        *
-        *          *
-        *          *
-         *        *
-            *  *
-
-
-  ------------------------
-
-  """
   test "no collision" do
-
     player = %{
       id: 0,
+      radius: 40,
       state: %{
-        x: 540,
-        y: 360,
+        x: 100,
+        y: 100
       }
     }
-    obstacle = %{a: %{x: 100, y: 100}, b: %{x: 800, y: 100}}
+
+    obstacle = %{a: %{x: 0, y: 200}, b: %{x: 200, y: 200}}
+
+    assert !CollisionDetector.collides(player, obstacle)
+  end
+
+  test "collision" do
+    player = %{
+      id: 0,
+      radius: 40,
+      state: %{
+        x: 100,
+        y: 200
+      }
+    }
+
+    obstacle = %{a: %{x: 0, y: 200}, b: %{x: 200, y: 200}}
+
+    assert CollisionDetector.collides(player, obstacle)
+  end
+
+  test "touch" do
+    player = %{
+      id: 0,
+      radius: 40,
+      state: %{
+        x: 100,
+        y: 100
+      }
+    }
+
+    obstacle = %{a: %{x: 0, y: 120}, b: %{x: 200, y: 120}}
 
     assert CollisionDetector.collides(player, obstacle)
   end
