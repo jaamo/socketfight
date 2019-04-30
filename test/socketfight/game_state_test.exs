8bb959ade04dd60309cd9bc24a0e2b34e4bf5da2
Jaakko Alajoki <jaamo@Jaakkos-MacBook-Pro.local>
2019-04-30 18:36:41 +0300

    Tests for situation where the line is completely inside the circle.

--- test/socketfight/game_state_test.exs
+++ test/socketfight/game_state_test.exs
@@ -46,4 +46,34 @@ defmodule AssertionTest do
 
     assert CollisionDetector.collides(player, obstacle)
   end
+
+  test "inside" do
+    player = %{
+      id: 0,
+      radius: 40,
+      state: %{
+        x: 100,
+        y: 100
+      }
+    }
+
+    obstacle = %{a: %{x: 0, y: 100}, b: %{x: 90, y: 100}}
+
+    assert CollisionDetector.collides(player, obstacle)
+  end
+
+  test "inside 2" do
+    player = %{
+      id: 0,
+      radius: 40,
+      state: %{
+        x: 100,
+        y: 100
+      }
+    }
+
+    obstacle = %{a: %{x: 80, y: 100}, b: %{x: 120, y: 100}}
+
+    assert CollisionDetector.collides(player, obstacle)
+  end
 end
